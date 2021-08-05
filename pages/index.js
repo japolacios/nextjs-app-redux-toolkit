de377e442ccdcfd694a47b58fa717e9ed4ef5f98
Japo <japolacios@gmail.com>
2021-08-04 22:29:57 -0500

    preserveAspectRatio Fix

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export default function Home() {
                 <a href="http://wikipedia.com">
                   <svg
                     className="featured-card__more-info-icon"
-                    preserveAspectRatio="true"
+                    preserveAspectRatio="xMinYMin meet"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 27 18"
                   >
